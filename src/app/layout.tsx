b44aa810670da4ca6486087ed71915cdf2c51a4c
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2023-12-07 14:14:36 +0600

    TESTED: Husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${inter.variable} ${spaceGrotesk.variable} `}>
         <Providers>
           <div>
-            <Navbar />
+            <Navba />
             {children}
             <Footer />
           </div>
